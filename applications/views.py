e0167e4d03d74635c6e4d74cf5e7fa1de454b22e
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-22 19:03:00 -0600

    add developer application view set

--- applications/views.py
+++ applications/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from .models import DeveloperApplication
+from .serializers import DeveloperApplicationSerializer
+
 
 # Create your views here.
+class DeveloperApplicationViewSet(ModelViewSet):
+    serializer_class = DeveloperApplicationSerializer
+    permission_classes = [IsAuthenticated]  # noqa: RUF012
+    http_method_names = ["get", "post", "patch", "delete", "head", "options"]  # noqa: RUF012
+
+    def get_queryset(self):
+        queryset = (
+            DeveloperApplication.objects.filter(
+                organization_memberships_user=self.request.user
+            )
+            .select_related("organization", "created_by")
+            .distinct()
+        )
+        
+        status_value = self.request.query_params.get("status")
+        
+        if status_value:
+            queryset = queryset.filter(status=status_value)
+            
+        return queryset
